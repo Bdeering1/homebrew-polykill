bb8c3c5ae180696a19cdd7eab16f515d5f29fc81
Bryn Deering <bdeering@uoguelph.ca>
2025-08-12 20:19:34 -0400

    Fixed formula style issues

--- Formula/polykill.rb
+++ Formula/polykill.rb
@@ -1,10 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Polykill < Formula
-  desc "Remove dependencies and build artifacts from unused local projects."
+  desc "Remove dependencies and build artifacts from unused local projects"
   homepage "https://crates.io/crates/polykill"
 
-  url "https://github.com/Bdeering1/polykill/archive/v1.5.2.tar.gz"
+  url "https://github.com/Bdeering1/polykill/archive/refs/tags/v1.5.2.tar.gz"
   sha256 "94b8c37d0db6fdeaeb0327711f2e657e36862171096e580b1d6be5c8e09e4b94"
   license "MIT"
 
